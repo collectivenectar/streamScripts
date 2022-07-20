16192f146ee6528e9395a33ca0a60dce9f45eb54
Jonathan Harvey <jonathanharvey22@gmail.com>
2022-07-20 18:11:18 +0200

    added views and public folders, consts and mongo string

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect(process.env.DB_CONNECTION,
+    {useNewUrlParser: true},
+    () => {console.log('Connected to database')}
+)
+
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
+    } catch (err) {
+        if (err) return response.status(500).send(err)
+    }
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
