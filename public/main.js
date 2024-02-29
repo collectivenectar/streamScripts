85ce01a03f0b79507e519581684cac414333441e
Jon Harvey <joncodes@proton.me>
2024-02-28 20:30:58 -0800

    Comments + Refactor: Added a few comments, prettier formatting, removed some unnecessary code and fixed self-closing tags

--- public/main.js
+++ public/main.js
@@ -1,83 +1,93 @@
-let playerButtons = document.getElementsByClassName('play-video')
-let iframeElement = document.getElementById('player-container').children[0]
+// grab all the buttons on the page that say 'go to this section'
+// it's for attaching a listener to them later
+let playerButtons = document.getElementsByClassName('play-video');
+// the player-container child is the video player iframe element
+let iframeElement = document.getElementById('player-container').children[0];
 
-const courseURLPrefix = "https://www.youtube-nocookie.com/embed/"
-// yt urls
+// first half of the url that gets fed to the iframe
+const courseURLPrefix = 'https://www.youtube-nocookie.com/embed/';
+// second half of the url that points to the specific yt video
 const courseURLSuffix = {
-  1: "o3IIobN4xR0",
-  2: "eCRbEILXXmE",
-  3: "rdWM6kUImjE",
-  4: "Q1Obtn29twk",
-  5: "E6Z8cWU_fjI",
-  6: "L55ax0blZY0",
-  7: "k8r3B0JGMt4",
-  8: "PWVRSXQxsXc",
-  9: "OaglXfjsBaE",
-  10: "WftjV2L7oyk",
-  11: "qEj0pXGVwjY",
-  12: "_A20kVsaqIk",
-  13: "pS6ykGL-fRE",
-  14: "cBWUvTZPeKw",
-  15: "68Li7ukgDKg",
-  16: "av6iPI_zJTU",
-  19: "YUQUGtUbwMY",
-  20: "cL0qP6kM_1U",
-  21: "75TQEQ6wxAE",
-  22: "LTda62-jyoM",
-  23: "qnmKELgyXc0",
-  24: "UpsonO_vBNk",
-  25: "ZlB4BockYNQ",
-  26: "WcSTeotmJtw",
-  27: "b5rjEW-_6po",
-  28: "G7XJRLaq2Cw",
-  29: "EOjUT746oLs",
-  30: "GihfY5OVDdk",
-  31: "PD-dx92RJtg",
-  32: "yEhs4XtuAgA",
-  33: "lIE1LFz4LJM",
-  34: "KM1RyffIKMg",
-  35: "IUCnAhr61pg",
-  36: "nv5SequVETI",
-  37: "6rsA_RCe5YM",
-  38: "000ai6I6Aow",
-  39: "zHq0v5RD_Zk",
-  40: "3eafTTnEfMw",
-  41: "LHf_STV_rLE",
-  44: "jZ-kmmgi_d0",
-  45: "jZ-kmmgi_d0",
-  46: "SVX_HMum0n4",
-  47: "SVX_HMum0n4",
+  1: 'o3IIobN4xR0',
+  2: 'eCRbEILXXmE',
+  3: 'rdWM6kUImjE',
+  4: 'Q1Obtn29twk',
+  5: 'E6Z8cWU_fjI',
+  6: 'L55ax0blZY0',
+  7: 'k8r3B0JGMt4',
+  8: 'PWVRSXQxsXc',
+  9: 'OaglXfjsBaE',
+  10: 'WftjV2L7oyk',
+  11: 'qEj0pXGVwjY',
+  12: '_A20kVsaqIk',
+  13: 'pS6ykGL-fRE',
+  14: 'cBWUvTZPeKw',
+  15: '68Li7ukgDKg',
+  16: 'av6iPI_zJTU',
+  19: 'YUQUGtUbwMY',
+  20: 'cL0qP6kM_1U',
+  21: '75TQEQ6wxAE',
+  22: 'LTda62-jyoM',
+  23: 'qnmKELgyXc0',
+  24: 'UpsonO_vBNk',
+  25: 'ZlB4BockYNQ',
+  26: 'WcSTeotmJtw',
+  27: 'b5rjEW-_6po',
+  28: 'G7XJRLaq2Cw',
+  29: 'EOjUT746oLs',
+  30: 'GihfY5OVDdk',
+  31: 'PD-dx92RJtg',
+  32: 'yEhs4XtuAgA',
+  33: 'lIE1LFz4LJM',
+  34: 'KM1RyffIKMg',
+  35: 'IUCnAhr61pg',
+  36: 'nv5SequVETI',
+  37: '6rsA_RCe5YM',
+  38: '000ai6I6Aow',
+  39: 'zHq0v5RD_Zk',
+  40: '3eafTTnEfMw',
+  41: 'LHf_STV_rLE',
+  44: 'jZ-kmmgi_d0',
+  45: 'jZ-kmmgi_d0',
+  46: 'SVX_HMum0n4',
+  47: 'SVX_HMum0n4',
+};
+
+// for each button on the page that says 'go to this section', add an event listener
+// and rig it to call the changePlayerUrl() function to update the url in the iFrame
+for (let player = 0; player < playerButtons.length; player++) {
+  playerButtons[player].addEventListener('click', changePlayerUrl);
 }
 
-for(let player = 0; player < playerButtons.length; player++){
-  playerButtons[player].addEventListener('click', changePlayerUrl)
+// This function grabs the timestamp off the element itself (it's populated through ejs)
+// which sits in the data-urlstring attr. Then give it to timeStampToMinutes() function
+// to convert into minutes for the YT timestamp API by setting src= to the new URL
+function changePlayerUrl(eventT) {
+  eventT.preventDefault();
+  let courseInfo = eventT.target.getAttribute('data-urlString').split('-');
+  let courseNum = courseInfo[0];
+  let courseTimeStamp = timeStampToMinutes(courseInfo[1]);
+  iframeElement.src = `${courseURLPrefix}${courseURLSuffix[courseNum]}?start=${courseTimeStamp}&origin=https://leonstreamscripts.herokuapp.com`;
+  return false;
 }
 
-function changePlayerUrl(eventT){
-  eventT.preventDefault()
-  let courseInfo = eventT.target.getAttribute('data-urlString').split("-")
-  let courseNum = courseInfo[0]
-  let courseTimeStamp = timeStampToMinutes(courseInfo[1])
-  iframeElement.src = `${courseURLPrefix}${courseURLSuffix[courseNum]}?start=${courseTimeStamp}&origin=https://leonstreamscripts.herokuapp.com`
-  return false
-}
-
-function timeStampToMinutes(timeStamp){
-  if(timeStamp.includes(":")){
-    let separate = timeStamp.split(":")
-    if(separate.length > 2){
-      let hours = Number(separate[0])
-      let minutes = Number(separate[1])
-      let seconds = Number(separate[2])
-      let total = seconds + (minutes * 60) + (hours * 60 * 60)
-      return String(total)
-    }else{
-      let minutes = Number(separate[0])
-      let seconds = Number(separate[1])
-      return String((minutes * 60) + seconds)
+// Timestamps come in the format `<course number>-<hours>:<minutes>:<seconds>`
+// Needs converting to purely seconds in order for YT API to accept it.
+function timeStampToMinutes(timeStamp) {
+  if (timeStamp.includes(':')) {
+    let separate = timeStamp.split(':');
+    if (separate.length > 2) {
+      let hours = Number(separate[0]);
+      let minutes = Number(separate[1]);
+      let seconds = Number(separate[2]);
+      let total = seconds + minutes * 60 + hours * 60 * 60;
+      return String(total);
+    } else {
+      let minutes = Number(separate[0]);
+      let seconds = Number(separate[1]);
+      return String(minutes * 60 + seconds);
     }
-  }
-  else{
-    return timeStamp
+  } else {
+    return timeStamp;
   }
 }
--- server.js
+++ server.js
@@ -1,206 +1,221 @@
 // set up variables
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8000
-const mongoose = require('mongoose')
-const Transcript = require('./models/transcriptModel.js')
-const path = require('path')
-const mongodb = require('mongodb');
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const mongoose = require('mongoose');
+const Transcript = require('./models/transcriptModel.js');
+const path = require('path');
+require('dotenv').config();
 
 // set up middleware
 
-app.set('view engine', 'ejs')
-// app.use(express.static('public'))
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.DB_CONNECTION, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, (error) => {
-  if(error){
-    console.log(error)
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log('Connected to database');
   }
-  console.log("Connected to database")
-})
+);
 
 // ROUTES
 
 // Main page
 
 app.get('/', async (request, response) => {
-    try {
-      response.render('index.ejs')
-    } catch (err) {
-        if (err) return response.status(500).send(err)
-    }
-})
+  try {
+    response.render('index.ejs');
+  } catch (err) {
+    if (err) return response.status(500).send(err);
+  }
+});
 
 // This setup is for regex search of any ENTRY field containing all words, in that order
-app.get("/search/entry/allOrdered", async (request, response) => {
-
+app.get('/search/entry/allOrdered', async (request, response) => {
   // Pagination setup
-  let perPage = 20
-  let page = request.query.page || 1
+  let perPage = 20;
+  let page = request.query.page || 1;
   // query setup
-  let lowerCasedEntry = String(request.query.entry).toLowerCase().trim().replace(/[^a-z0-9 ]/gi,'')
-  if(lowerCasedEntry === ""){
-    lowerCasedEntry = '___'
+  let lowerCasedEntry = String(request.query.entry)
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 ]/gi, '');
+  if (lowerCasedEntry === '') {
+    lowerCasedEntry = '___';
   }
-  let query = {"entry": {$regex: String(lowerCasedEntry)}}
-  let sort = {_id: 1}
+  let query = { entry: { $regex: String(lowerCasedEntry) } };
+  let sort = { _id: 1 };
   // query to mongodb
-  Transcript.find(query).sort(sort).skip((perPage * page) - perPage).limit(perPage)
-  .exec((error, transcriptsList) => {
-    Transcript.countDocuments(query).exec((error, count) => {
-      if(error) return next(error)
-      if(count === 0){
-        response.render('results.ejs', {
+  Transcript.find(query)
+    .sort(sort)
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((error, transcriptsList) => {
+      Transcript.countDocuments(query).exec((error, count) => {
+        if (error) return next(error);
+        if (count === 0) {
+          response.render('results.ejs', {
             transcripts: {
-              timestamp: "0-0",
-              entry: `${request.query.entry}`
+              timestamp: '0-0',
+              entry: `${request.query.entry}`,
             },
             current: 1,
             pages: 1,
             perPage: 1,
             entry: `${request.query.entry}`,
-            urlBase: "/search/entry/allOrdered",
-            count: 0
-        })
-      } else{
-        response.render('results.ejs', {
-          transcripts: transcriptsList,
-          current: page,
-          pages: Math.ceil(count / perPage),
-          perPage: perPage,
-          entry: request.query.entry,
-          urlBase: "/search/entry/allOrdered",
-          count: count
-        })
-      }
-    })
-  })
-})
+            urlBase: '/search/entry/allOrdered',
+            count: 0,
+          });
+        } else {
+          response.render('results.ejs', {
+            transcripts: transcriptsList,
+            current: page,
+            pages: Math.ceil(count / perPage),
+            perPage: perPage,
+            entry: request.query.entry,
+            urlBase: '/search/entry/allOrdered',
+            count: count,
+          });
+        }
+      });
+    });
+});
 
 // This setup is for regex search of any ENTRY field containing all of the
 // words (except allows any order)
-app.get("/search/entry/allUnordered", async (request, response) => {
-
+app.get('/search/entry/allUnordered', async (request, response) => {
   // Pagination setup
-  let perPage = 20
-  let page = request.query.page || 1
+  let perPage = 20;
+  let page = request.query.page || 1;
 
   // query setup
-  const queryString = String(request.query.entry).toLowerCase().trim().replace(/[^a-z0-9 ]/gi,'')
-  let allQueries = []
-  if(queryString === ""){
-    allQueries = [{"entry": {$regex: String("___")}}]
-  } else{
-    let splitQuery = queryString.split(" ")
-    splitQuery.forEach(element => {
-      allQueries.push({"entry": {$regex: String(element)}})
-    })
+  const queryString = String(request.query.entry)
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 ]/gi, '');
+  let allQueries = [];
+  if (queryString === '') {
+    allQueries = [{ entry: { $regex: String('___') } }];
+  } else {
+    let splitQuery = queryString.split(' ');
+    splitQuery.forEach((element) => {
+      allQueries.push({ entry: { $regex: String(element) } });
+    });
   }
 
-  const query = {$and: allQueries}
-  let sort = {_id: 1}
+  const query = { $and: allQueries };
+  let sort = { _id: 1 };
 
   // query to mongodb
-  Transcript.find(query).sort(sort).skip((perPage * page) - perPage).limit(perPage)
-  .exec((error, transcriptsList) => {
-    Transcript.countDocuments(query).exec((error, count) => {
-      if(error) return next(error)
-      if(count == 0){
-        response.render('results.ejs', {
-          transcripts: {
-              timestamp: "0-0",
-              entry: `${request.query.entry}`
+  Transcript.find(query)
+    .sort(sort)
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((error, transcriptsList) => {
+      Transcript.countDocuments(query).exec((error, count) => {
+        if (error) return next(error);
+        if (count == 0) {
+          response.render('results.ejs', {
+            transcripts: {
+              timestamp: '0-0',
+              entry: `${request.query.entry}`,
             },
             current: 1,
             pages: 1,
             perPage: 1,
             entry: `${request.query.entry}`,
-            urlBase: "/search/entry/allUnordered",
-            count: 0
-        })
-      } else{
-        response.render('results.ejs', {
-          transcripts: transcriptsList,
-          current: page,
-          pages: Math.ceil(count / perPage),
-          perPage: perPage,
-          entry: request.query.entry,
-          urlBase: "/search/entry/allUnordered",
-          count: count
-        })
-      }
-    })
-  })
-})
+            urlBase: '/search/entry/allUnordered',
+            count: 0,
+          });
+        } else {
+          response.render('results.ejs', {
+            transcripts: transcriptsList,
+            current: page,
+            pages: Math.ceil(count / perPage),
+            perPage: perPage,
+            entry: request.query.entry,
+            urlBase: '/search/entry/allUnordered',
+            count: count,
+          });
+        }
+      });
+    });
+});
 
 // This setup is for regex search of any TIMESTAMP related to a course number (1-41 currently)
-app.get("/search/timestamp/byCourseNumber", async (request, response) => {
-
+app.get('/search/timestamp/byCourseNumber', async (request, response) => {
   // Pagination setup
-  let perPage = 20
-  let page = request.query.page || 1
+  let perPage = 20;
+  let page = request.query.page || 1;
 
   // query setup
-  const courseNumber = request.query.entry
-  let search
-  if(Number(courseNumber)){
-    search = "^" + courseNumber + "-"
-  } else{
-    search = "^" + 1 + "-"
+  const courseNumber = request.query.entry;
+  let search;
+  if (Number(courseNumber)) {
+    search = '^' + courseNumber + '-';
+  } else {
+    search = '^' + 1 + '-';
   }
-  const query = {timestamp: new RegExp(search)}
-  let sort = {_id: 1}
-  Transcript.find(query).sort(sort).skip((perPage * page) - perPage).limit(perPage)
-  .exec((error, transcriptsList) => {
-    Transcript.countDocuments(query).exec((error, count) => {
-      if(error) return next(error)
-      if(count === 0){
-        response.render('results.ejs', {
+  const query = { timestamp: new RegExp(search) };
+  let sort = { _id: 1 };
+  Transcript.find(query)
+    .sort(sort)
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((error, transcriptsList) => {
+      Transcript.countDocuments(query).exec((error, count) => {
+        if (error) return next(error);
+        if (count === 0) {
+          response.render('results.ejs', {
             transcripts: {
-              timestamp: "0-0",
-              entry: `${request.query.entry}`
+              timestamp: '0-0',
+              entry: `${request.query.entry}`,
             },
             current: 1,
             pages: 1,
             perPage: 1,
             entry: `${request.query.entry}`,
-            urlBase: "/search/timestamp/byCourseNumber",
-            count: "0"
-        })
-      } else{
-        response.render('results.ejs', {
-          transcripts: transcriptsList,
-          current: page,
-          pages: Math.ceil(count / perPage),
-          perPage: perPage,
-          entry: request.query.entry,
-          urlBase: "/search/timestamp/byCourseNumber",
-          count: count
-        })
-      }
-    })
-  })
-})
+            urlBase: '/search/timestamp/byCourseNumber',
+            count: '0',
+          });
+        } else {
+          response.render('results.ejs', {
+            transcripts: transcriptsList,
+            current: page,
+            pages: Math.ceil(count / perPage),
+            perPage: perPage,
+            entry: request.query.entry,
+            urlBase: '/search/timestamp/byCourseNumber',
+            count: count,
+          });
+        }
+      });
+    });
+});
 
 // Browse by course, browse by topic?
 
 app.get('/browse', async (request, response) => {
-    try {
-        response.render('browse.ejs')
-    } catch (err) {
-        if (err) return response.status(500).send(err)
-    }
-})
-
+  try {
+    response.render('browse.ejs');
+  } catch (err) {
+    if (err) return response.status(500).send(err);
+  }
+});
 
 // listen to routes
 
-app.listen(process.env.PORT || PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
+);
